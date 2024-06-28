412315b806870f99a3839abb0192c49cc8204fd1
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2024-06-28 18:27:39 +0700

    Update for reports output

--- src/test/java/org/ndviet/runner/CucumberRunnerTests.java
+++ src/test/java/org/ndviet/runner/CucumberRunnerTests.java
@@ -1,14 +1,25 @@
 package org.ndviet.runner;
 
+import io.cucumber.tagexpressions.TagExpressionParser;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.PickleWrapper;
+import org.testng.annotations.DataProvider;
 
-@CucumberOptions(
-        tags = "",
-        features = "src/test/resources/features",
-        glue = "org.ndviet.definitions",
-        plugin = {}
-)
+import java.util.Arrays;
+
+@CucumberOptions( features = "src/test/resources/features",
+        glue = { "org.ndviet.definitions" },
+        tags = "")
 
 public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        Object[][] scenarios = super.scenarios();
+        String dynamicTags = System.getenv("includes");
+        if (dynamicTags != null && !dynamicTags.isEmpty())
+            return (Arrays.stream(scenarios).filter(scenario -> TagExpressionParser.parse(dynamicTags).evaluate(((PickleWrapper) scenario[0]).getPickle().getTags())).toList()).toArray(new Object[0][0]);
+        else return scenarios;
+    }
 }
